f3159dd5da1ea26a600b0a6cc08d650b244da5e1
Suraj kumar <ksurajkumar336@gmail.com>
2026-07-29 12:08:56 +0530

    add new file

---
+++ Practice/basic6.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class basic6 {
+  public static void main(String[]args) {
+    
+  }
+
+}
--- basic5.java
+++ basic5.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+// Reverse a number 
+
 public class basic5 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter number : ");
+    int num = sc.nextInt();
+   
+
+    int reverse = 0;
+    while (num!=0) {
+      int digit = num % 10;
+      reverse = reverse * 10 + digit;
+      num = num / 10;
+    } 
+
+    System.out.println("reverse number is : " + reverse);
 
   }
 }
